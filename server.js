17c3443740c006c03fada221e6903741ca69082f
swee-27 <swetha.i2023cse@sece.ac.in>
2025-10-16 10:28:41 +0530

    commit

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(3000,'0.0.0.0', () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
